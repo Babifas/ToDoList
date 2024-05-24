e1c4d09889c80a699e667f7ed1c7b8b9a198a908
BABIFAS <babifasp@gmail.com>
2024-05-24 21:35:11 +0530

    Add project files.

---
+++ ToDoList/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static List<ToDoItem> tasks = new List<ToDoItem>
+        {
+            new ToDoItem { Id=123,Title="to complete c#",Description="don't miss it",DueDate=DateTime.UtcNow.AddDays(6),IsCompleted=false}
+        };
+        public IActionResult Index()
+        {
+            return View(tasks);
+        }
+    }
+}
